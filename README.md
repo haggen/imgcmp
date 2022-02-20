035dffffc7019e6d4f46c034492608ff27b9ea0a
Arthur <arthur@corenzan.com>
2022-02-19 22:11:15 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Image compare slider
+
+> Easy visual image comparison.
+
+## License
+
+Apache-2.0 © 2022 Arthur Corenzan
---
+++ src/script.js
@@ -0,0 +1,61 @@
+document.body.addEventListener(
+  "dragenter",
+  (event) => {
+    event.target.classList.add("dropzone");
+  },
+  false
+);
+
+document.body.addEventListener(
+  "dragleave",
+  (event) => {
+    event.target.classList.remove("dropzone");
+  },
+  false
+);
+
+document.body.addEventListener(
+  "dragover",
+  (event) => {
+    event.preventDefault();
+  },
+  false
+);
+
+let nextImageIndex = 0;
+
+/**
+ *
+ * @param {File} file
+ */
+const setNextImage = (file) => {
+  if (!/image.*/.test(file.type)) {
+    throw new Error("Not an image");
+  }
+  document.images[nextImageIndex].src = URL.createObjectURL(file);
+  nextImageIndex = (nextImageIndex + 1) % document.images.length;
+};
+
+document.body.addEventListener(
+  "drop",
+  (event) => {
+    event.preventDefault();
+    event.target.classList.remove("dropzone");
+    for (const file of event.dataTransfer.files) {
+      setNextImage(file);
+    }
+  },
+  false
+);
+
+document.body.addEventListener("mousemove", (event) => {
+  const target = document.images[1];
+  const clampedRelativeX = Math.min(
+    Math.max(0, event.clientX - target.offsetLeft),
+    target.offsetWidth
+  );
+
+  target.style.clipPath = `inset(0 ${
+    target.offsetWidth - clampedRelativeX
+  }px 0 0)`;
+});
